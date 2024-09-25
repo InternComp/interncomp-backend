e1b15476c139e3b22f2787c83708b44b6967ed66
aaronoates <aaronoates2003@gmail.com>
2024-09-25 16:04:49 -0230

    routes example
    an example of how to use routes with get http requests

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 const express = require('express');
 const app = express();
 
-//Example defining a route in Express
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, Express.js server!</h1>');
+    res.send('hello root node'); // this gets executed when you run http://localhost:3000/
 });
 
-//Example specifying the port and starting the server
-const port = process.env.PORT || 3000; //You can use environment variables for port configuration
+//Include route file
+const usersRoute = require('./routes/users');
+
+//Use the route
+
+app.use('/users', usersRoute);
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
