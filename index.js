e4c59260dc8da075379a4c506f9231a06e5d7acc
aaronoates <aaronoates2003@gmail.com>
2024-11-06 15:02:59 -0330

    Job post api works

--- index.js
+++ index.js
@@ -95,16 +95,13 @@ app.get('/companies',async(req,res) =>{
 
 // Create a new Job
 app.post('/jobs', async (req, res) => {
-    const { description, linkedin, recruiterEmail, careerPages, salaryInfo, skillsRequired } = req.body;
+    const { description, linkedin, skillsRequired } = req.body;
   
     try {
         const newJob = await prisma.job.create({
             data: {
                 description,
                 linkedin,
-                recruiterEmail,
-                careerPages,
-                salaryInfo,
                 skillsRequired
             },
         });
